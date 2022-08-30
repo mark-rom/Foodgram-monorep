cbb675a4bd41b438944a8d39f3e47c402dfe3931
Павел Сергеев <pavel.al.sergeev@gmail.com>
2022-08-30 16:32:07 +0300

    split into several functions

--- backend/foodgram/api/shopping_list_pdf.py
+++ backend/foodgram/api/shopping_list_pdf.py
@@ -1,25 +1,67 @@
+import io
+
 from django.conf import settings
-from fpdf import FPDF
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 
-pdf = FPDF(orientation='P', format='A4')
-
-
-def get_text(queryset):
-    text = ''
-    for dictionary in queryset:
-        name, unit, amount = dictionary.values()
-        text += ' '.join((name, str(amount), unit, '\n'))
-    return text
+font_path = settings.BASE_DIR / 'backend_static/data/fonts/DejaVuSans.ttf'
 
 
-def get_pdf(text):
-    txt = get_text(text)
-    pdf.add_font(
-        'DejaVuSans', '',
-        fname=settings.BASE_DIR.parent.parent / 'data/fonts/DejaVuSans.ttf',
-        uni=True
-    )
-    pdf.set_font('DejaVuSans', size=14)
-    pdf.add_page()
-    pdf.cell(w=20, h=20, txt=txt)
-    return pdf.output(dest='S').encode('utf-8')
+def create_pdf(buffer, x_pt, y_pt):
+
+    pdf = canvas.Canvas(buffer)
+    pdfmetrics.registerFont(TTFont(
+        'DejaVuSans',
+        font_path,
+        'utf-8'
+    ))
+    pdf.setFont('DejaVuSans', 14)
+    pdf.drawString(x_pt, y_pt, 'Список покупок')
+
+    return pdf
+
+
+def fill_pdf(pdf, ingredients_qs, x_pt, y_pt):
+
+    for ingredient in ingredients_qs:
+
+        name, unit, amount = ingredient.values()
+
+        if y_pt < 57 and x_pt == 382:
+            pdf.showPage()
+            x_pt = 85
+            y_pt = 785
+            pdf.setFont('DejaVuSans', 12)
+
+        elif y_pt <= 57:
+            x_pt = 382
+            y_pt = 749
+
+        pdf.drawString(x_pt, y_pt, ' '.join((name, str(amount), unit)))
+        y_pt -= 28
+
+
+def get_shopping_list(shopping_list):
+
+    x_pt = 85
+    y_pt = 785
+
+    buffer = io.BytesIO()
+    pdf = create_pdf(buffer, x_pt, y_pt)
+
+    pdf.setFont('DejaVuSans', 12)
+    y_pt -= 36
+
+    if len(shopping_list) < 1:
+        pdf.drawString(
+            x_pt, y_pt, 'Вы еще не добавили рецепты в список покупок.'
+        )
+
+    else:
+        fill_pdf(pdf, shopping_list, x_pt, y_pt)
+
+    pdf.showPage()
+    pdf.save()
+    buffer.seek(0)
+    return buffer
